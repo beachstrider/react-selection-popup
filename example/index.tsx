7476491831f6798e770e3c76523e4c98d3d36e16
Jason Mendoza <mernstackdev729@gmail.com>
2023-06-12 15:07:22 -0400

    chore: release v0.1.0

---
+++ example/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ReactSelectionPopup from '../src'
+
+const App = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <ReactSelectionPopup
+        selectionClassName="selection"
+        multipleSelection={false}
+        offsetToTop={5}
+        metaAttrName="data-meta"
+        onSelect={(text, meta) => console.debug(text, meta)}
+      >
+        <div style={{ background: 'yellow' }}>Sample Popup</div>
+      </ReactSelectionPopup>
+      <p className="selection" data-meta={JSON.stringify({ explain: 'Test text' })}>
+        Select some text to see the popup.
+      </p>
+    </div>
+  )
+}
+
+const container = document.getElementById('root') as Element
+const root = ReactDOM.createRoot(container)
+
+root.render(<App />)
---
+++ src/index.tsx
@@ -0,0 +1,161 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+interface ReactSelectionPopupProps {
+  /**
+   * This function is called when a user selects texts in html.
+   * @param text - The text of the selection
+   * @param meta - Additional metadata associated with the selected text (optional)
+   */
+  onSelect?: (text: string, meta?: string | number | boolean | object) => void
+  /**
+   * The child elements to be displayed within the popup component.
+   */
+  children: React.ReactNode
+  /**
+   * The className to be used to identify selectable element(s).
+   */
+  selectionClassName: string
+  /**
+   * Whether multiple elements can be selected at once (default is false).
+   */
+  multipleSelection?: boolean
+  /**
+   * The name of the metadata attribute associated with the selected text (optional).
+   * The metadata value should be JSON stringfied.
+   * @example <div ... className="selection" metaAttrName="data-meta">...</div>
+   * ...
+   * <ReactSelectionPopup ... data-meta={JSON.stringfy(data)}>...</ReactSelectionPopup>
+   */
+  metaAttrName?: string
+  /**
+   * The offset (in pixels) to the left direction of the screen to reposition the popup. The default pivot x is right of the pop.
+   */
+  offsetToLeft?: number
+  /**
+   * The offset (in pixels) to the top direction of the screen to reposition the popup. The default pivot y is bottom of the pop.
+   */
+  offsetToTop?: number
+}
+
+type Size = {
+  /**
+   * The width of the element in pixels.
+   */
+  width: number
+  /**
+   * The height of the element in pixels.
+   */
+  height: number
+}
+
+type Position = {
+  /**
+   * The x-coordinate of the upper-left corner of the element.
+   */
+  x: number
+  /**
+   * The y-coordinate of the upper-left corner of the element.
+   */
+  y: number
+}
+
+const ReactSelectionPopup = ({
+  onSelect,
+  children,
+  selectionClassName,
+  multipleSelection = true,
+  metaAttrName,
+  offsetToLeft = 0,
+  offsetToTop = 0
+}: ReactSelectionPopupProps) => {
+  const [size, setSize] = useState<Size>({ width: 0, height: 0 })
+  const [position, setPosition] = useState<Position | null>(null)
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    window.addEventListener('mouseup', (e: any) => {
+      const selection = window.getSelection()
+      if (selection !== null) {
+        const { anchorNode, focusNode } = selection
+
+        if (anchorNode !== null && focusNode !== null) {
+          if (anchorNode.parentElement !== null && anchorNode.parentElement.classList.contains(selectionClassName)) {
+            const text = selection.toString()
+            const meta = JSON.parse(e.target.getAttribute(metaAttrName))
+
+            if (text) {
+              if (!metaAttrName || meta) {
+                if (selection.rangeCount !== 0) {
+                  if (anchorNode.isEqualNode(focusNode) || multipleSelection) {
+                    const range = selection.getRangeAt(0)
+
+                    const { right: x, top: y } = range.getBoundingClientRect()
+
+                    // TODO: position {x, y} should come from the first line of selection
+
+                    setPosition({ x, y })
+                    onSelect?.(text, meta)
+
+                    return
+                  } else {
+                    selection.removeAllRanges()
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        setPosition(null)
+      }
+    })
+
+    window.addEventListener('mousedown', (e) => {
+      const selection = window.getSelection()
+
+      let node: HTMLElement | null = e.target as HTMLElement
+
+      // Check if the target div is popup which is the exception case
+      while (node != null) {
+        if (node === ref.current) {
+          return
+        }
+
+        node = node.parentNode as HTMLElement
+      }
+
+      if (selection !== null) {
+        selection.removeAllRanges()
+      }
+
+      setPosition(null)
+    })
+
+    window.addEventListener('scroll', () => {
+      setPosition(null)
+    })
+  }, [onSelect, multipleSelection, selectionClassName, metaAttrName])
+
+  useEffect(() => {
+    if (ref.current) {
+      const width = ref.current.offsetWidth
+      const height = ref.current.offsetHeight
+
+      setSize({ width, height })
+    }
+  }, [children, position])
+
+  if (position === null) return <></>
+
+  const left = position.x - size.width - offsetToLeft
+  const top = position.y - size.height - offsetToTop
+
+  return (
+    <div style={{ position: 'fixed', left, top }}>
+      <div ref={ref}>{children}</div>
+    </div>
+  )
+}
+
+export default ReactSelectionPopup
